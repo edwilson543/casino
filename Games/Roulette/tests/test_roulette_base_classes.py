49417eb8aef2a789e98a82ba6483df01afea0d28
edwilson543 <edward.e.j.wilson@gmail.com>
2022-03-29 20:43:49 +0100

    minor_updates

--- Games/Roulette/tests/test_roulette_base_classes.py
+++ Games/Roulette/tests/test_roulette_base_classes.py
@@ -3,7 +3,8 @@ from Games.Roulette.definitions.wheel_defns import wheel_options
 euro_wheel = wheel_options['E']
 
 
-# TODO change this so that we use some test wheel T which is defined in the wheel defn.s page
+# TODO write more tests - also find out how to write tests which navigate the game, rather than having to play it
+# probably one to wait until more of a finished article
 
 class TestRouletteWheel:
     def test_wheel_size_euro_wheel(self):
