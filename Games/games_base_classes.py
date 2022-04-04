29c33162fb6d0bfc00b0eaa3717c49d8276fdf27
edwilson543 <edward.e.j.wilson@gmail.com>
2022-04-04 08:49:52 +0100

    added a games_base_classes.py

---
+++ Games/games_base_classes.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+
+class Player:
+    def __init__(self,
+                 name: str,
+                 password: str,
+                 initial_pot: int,
+                 initial_pot_datetime: datetime,
+                 active_pot: int):
+        self.name = name
+        self.password = password
+        self.initial_pot = initial_pot
+        self.initial_pot_datetime = initial_pot_datetime
+        self.active_pot = active_pot
+
+    def get_profit_report(self):
+        print(f"Your current pot is £{self.active_pot}.\n"
+              f"Since {str(self.initial_pot_datetime)}, you have {self.won_or_lost()}:"
+              f"{abs(self.active_pot - self.initial_pot)}")
+
+    def won_or_lost(self):
+        if self.initial_pot > self.active_pot:
+            return "lost"
+        else:
+            return "won"
