5ee0a231a4cd7df0e1d94bc41b9d726c5cc4036d
edwilson543 <edward.e.j.wilson@gmail.com>
2022-04-05 07:20:38 +0100

    got rid of payout scaler in favour of a bias_colour attribute and bias_colour_wheel size in the RouletteWheel in roulette_base_classes.py

--- Games/Roulette/definitions/wheel_defns.py
+++ Games/Roulette/definitions/wheel_defns.py
@@ -3,20 +3,18 @@ from Games.Roulette.app.roulette_base_classes import RouletteWheel
 """
 To define a new wheel complete the following steps:
 1) Create wheel_defn as a mapping of the numbers on the wheel to the colours
-2) Define a payout scaler. payout := stake * payout_scaler / P(bet winning)
-3) Add some colour ids - these should be unique, using as many characters as necessary e.g. BLU and BLA if blue and
+2) Add some colour ids - these should be unique, using as many characters as necessary e.g. BLU and BLA if blue and
 black feature on the same wheel
-4) Define a colour_options_text for that wheel, which should correspond to the colour ids 
+3) Define a colour_options_text for that wheel, which should correspond to the colour ids 
 5) Define the wheel_colour_bias - this is the colour that gets ignored when calculating probabilities
-5) Create a subclass of the RouletteWheel base class for each wheel, using the defined parameters
-6) Add the wheel to the wheel_options dict below, using an identification key
-7) Add the wheel option to the wheel_options_text so that it can be chosen in the game, with the same key
-8) Add relevant bet type options to the wheel in bet_type_defns, using the wheel id key from wheel_options
-9) Map relevant bet type methods for the wheel in roulette_bas_classes RouletteWheel class, defining any new methods
+6) Create a subclass of the RouletteWheel base class for each wheel, using the defined parameters
+7) Add the wheel to the wheel_options dict below, using an identification key
+8) Add the wheel option to the wheel_options_text so that it can be chosen in the game, with the same key
+9) Add relevant bet type options to the wheel in bet_type_defns, using the wheel id key from wheel_options
+10) Map relevant bet type methods for the wheel in roulette_base_classes RouletteWheel class, defining any new methods
 if necessary.
 """
 # TODO add dictionaries for the boards to the definitions, maybe with a string rep of form "|1|2|\n-----\n|3|4|"
-# TODO add the colour bias attribute for each wheel
 ###############################
 # Wheel definitions and payouts
 ###############################
@@ -25,27 +23,24 @@ euro_wheel_defn = {0: 'green', 1: 'red', 2: 'black', 3: 'red', 4: 'black', 5: 'r
                    16: 'red', 17: 'black', 18: 'red', 19: 'red', 20: 'black', 21: 'red', 22: 'black', 23: 'red',
                    24: 'black', 25: 'red', 26: 'black', 27: 'red', 28: 'black', 29: 'black', 30: 'red', 31: 'black',
                    32: 'red', 33: 'black', 34: 'red', 35: 'black', 36: 'red'}
-euro_wheel_payout_scaler = 0.95
 euro_wheel_colour_ids = {'G': 'green', 'R': 'red', 'B': 'black'}
 euro_wheel_colour_options_text = "[G]reen, [R]ed, [B]lack"
-euro_wheel_colour_bias = 'green'
+euro_wheel_bias_colour = 'green'
 
 american_wheel_defn = {-1: 'green', 0: 'green', 1: 'red', 2: 'black', 3: 'red', 4: 'black', 5: 'red', 6: 'black',
                        7: 'red', 8: 'black', 9: 'red', 10: 'black', 11: 'black', 12: 'red', 13: 'black', 14: 'red',
                        15: 'black', 16: 'red', 17: 'black', 18: 'red', 19: 'red', 20: 'black', 21: 'red', 22: 'black',
                        23: 'red', 24: 'black', 25: 'red', 26: 'black', 27: 'red', 28: 'black', 29: 'black', 30: 'red',
                        31: 'black', 32: 'red', 33: 'black', 34: 'red', 35: 'black', 36: 'red'}
-american_wheel_payout_scaler = 0.95
 # note -1 corresponds to 00, which is in effect the same as 0
 american_wheel_colour_ids = {'G': 'green', 'R': 'red', 'B': 'black'}
 american_wheel_colour_options_text = "[G]reen, [R]ed, [B]lack"
-american_wheel_colour_bias = 'green'
+american_wheel_bias_colour = 'green'
 
 template_wheel_defn = {0: 'black', 1: 'white'}
-template_wheel_payout_scaler = 0.95
 template_wheel_colour_ids = {'B': 'black', 'W': 'white'}
 template_wheel_colour_options_text = "[B]lack, [W]hite"
-template_wheel_colour_bias = 'black'
+template_wheel_bias_colour = 'black'
 
 
 ###############################
@@ -54,31 +49,28 @@ template_wheel_colour_bias = 'black'
 class EuroWheel(RouletteWheel):
     def __init__(self):
         slots = euro_wheel_defn
-        payout_scaler = euro_wheel_payout_scaler
         colour_ids = euro_wheel_colour_ids
         colour_options = euro_wheel_colour_options_text
-        colour_bias = euro_wheel_colour_bias
-        super().__init__(slots, payout_scaler, colour_ids, colour_options, colour_bias)
+        bias_colour = euro_wheel_bias_colour
+        super().__init__(slots, colour_ids, colour_options, bias_colour)
 
 
 class AmericanWheel(RouletteWheel):
     def __init__(self):
         slots = american_wheel_defn
-        payout_scaler = american_wheel_payout_scaler
         colour_ids = american_wheel_colour_ids
         colour_options = american_wheel_colour_options_text
-        colour_bias = american_wheel_colour_bias
-        super().__init__(slots, payout_scaler, colour_ids, colour_options, colour_bias)
+        bias_colour = american_wheel_bias_colour
+        super().__init__(slots, colour_ids, colour_options, bias_colour)
 
 
 class TemplateWheel(RouletteWheel):
     def __init__(self):
         slots = template_wheel_defn
-        payout_scaler = template_wheel_payout_scaler
         colour_ids = template_wheel_colour_ids
         colour_options = template_wheel_colour_options_text
-        colour_bias = template_wheel_colour_bias
-        super().__init__(slots, payout_scaler, colour_ids, colour_options, colour_bias)
+        bias_colour = template_wheel_bias_colour
+        super().__init__(slots, colour_ids, colour_options, bias_colour)
 
 
 ##############################
