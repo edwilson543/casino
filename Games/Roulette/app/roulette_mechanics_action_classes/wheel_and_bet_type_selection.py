70e5f88c2364fc24f9121a5040b0986dac164e8e
edwilson543 <edward.e.j.wilson@gmail.com>
2022-04-21 08:02:49 +0100

    added some generic Typevars for the return of the lookup function to resolve the issue where downstream processing is expecting the wrong tpye of bet/wheel in the class hierarchy

--- Games/Roulette/app/roulette_mechanics_action_classes/wheel_and_bet_type_selection.py
+++ Games/Roulette/app/roulette_mechanics_action_classes/wheel_and_bet_type_selection.py
@@ -1,8 +1,15 @@
-from Games.Roulette.app.roulette_wheel_base_class import RouletteWheel
-from Games.Roulette.app.roulette_bet_base_class import RouletteBet
-
-
 #  TODO change the look up methods to not be from a dictionary
+from Games.Roulette.app.roulette_bet_base_class import RouletteBet
+from Games.Roulette.app.roulette_bet_base_class import RouletteWheel
+from typing import TypeVar
+"""
+Typevars used to specify returns from the look up functions
+e.g. return from the wheel lookup is at most a RouletteWheel, or any subclass of RouletteWheel 
+"""
+WHEEL_TYPES = TypeVar(name="WHEEL_TYPES", bound=RouletteWheel)
+BET_TYPES = TypeVar(name="BET_TYPES", bound=RouletteBet)
+
+
 class WheelAndBetTypeSelector:
     """Class to look up wheel and bet objects based on their ids, and also to determine all in status"""
 
@@ -12,24 +19,26 @@ class WheelAndBetTypeSelector:
         self.wheel_look_up = wheel_look_up
         self.bet_type_look_up = bet_type_look_up
 
-    def get_wheel_from_wheel_id(self, wheel_id: str) -> RouletteWheel:
+    def get_wheel_from_wheel_id(self, wheel_id: str) -> WHEEL_TYPES:
         """
         Method to take the wheel_id and return a live wheel object (subclass).
         Parameters: wheel_id - this is a string, e.g. 'E' in the case of the european wheel.
         Returns: a subclass of RouletteWheel, which has all the instance attributes defined (e.g. Euro_wheel().
         """
         if wheel_id in self.wheel_look_up:
-            return self.wheel_look_up[wheel_id]
+            wheel = self.wheel_look_up[wheel_id]
+            return wheel
         else:
             raise NameError(f"No wheel with id {wheel_id} found.")
 
-    def get_bet_type_from_bet_type_id(self, bet_type_id: str) -> RouletteBet:
+    def get_bet_type_from_bet_type_id(self, bet_type_id: str) -> BET_TYPES:
         """
         Method to take the bet_type_id and return a live bet object (subclass).
         Parameters: bet_type_id - string, e.g. 'C' which represents ColoursBet subclass of Bet
         Returns: A subclass of Bet which is a fully defined bet class (i.e. includes bet placing).
         """
         if bet_type_id in self.bet_type_look_up:
-            return self.bet_type_look_up[bet_type_id]
+            bet_type = self.bet_type_look_up[bet_type_id]
+            return bet_type
         else:
             raise NameError(f"No bet type with id {bet_type_id} found.")
--- user_interface/command_line/Roulette/app/roulette_mechanics_user_classes/wheel_and_bet_type_selection_user.py
+++ user_interface/command_line/Roulette/app/roulette_mechanics_user_classes/wheel_and_bet_type_selection_user.py
@@ -1,11 +1,19 @@
-from Games.games_base_classes import Player
-from Games.Roulette.app.roulette_bet_base_class import RouletteBet
-from Games.Roulette.app.roulette_wheel_base_class import RouletteWheel
-from Games.Roulette.app.roulette_mechanics_action_classes.bet_selection import WheelAndBetTypeSelector
-
+from Games.Roulette.app.roulette_mechanics_action_classes.wheel_and_bet_type_selection import WheelAndBetTypeSelector
+from user_interface.command_line.Roulette.app.roulette_bet_base_class_user import RouletteBetUser
+from user_interface.command_line.Roulette.app.roulette_wheel_base_class_user import RouletteWheelUser
 from user_interface.command_line.Roulette.definitions.wheel_defns_user import wheel_options_text
 from user_interface.command_line.Roulette.definitions.bet_type_defns_user import bet_cats_and_types, \
     bet_cat_options_text, bet_type_options_text
+
+from typing import TypeVar
+
+"""
+Typevars used to specify returns from the look up functions
+e.g. return from the wheel lookup is at most a RouletteWheelUser, or any subclass of RouletteWheel 
+"""
+
+USER_WHEEL_TYPES = TypeVar(name="USER_WHEEL_TYPES", bound=RouletteWheelUser)
+USER_BET_TYPES = TypeVar(name="USER_BET_TYPES", bound=RouletteBetUser)
 
 
 class WheelAndBetTypeSelectorUser(WheelAndBetTypeSelector):
@@ -14,25 +22,24 @@ class WheelAndBetTypeSelectorUser(WheelAndBetTypeSelector):
 
     def __init__(self,
                  wheel_look_up: dict,
-                 bet_type_look_up: dict,
-                 active_player: Player):
+                 bet_type_look_up: dict):
         super().__init__(wheel_look_up, bet_type_look_up)
-        self.active_player = active_player
 
-    def choose_playing_wheel(self) -> (str, RouletteWheel):
+    def choose_playing_wheel(self) -> (str, USER_WHEEL_TYPES):
         """
         Method to navigate the user through the wheel options and return a live wheel object.
         Returns: the wheel_id, and the associated relevant subclass of RouletteWheel defining that wheel
         """
         while True:
-            wheel_choice = input("What wheel would you like to play on?\n"
-                                 f"{wheel_options_text}\n--->").upper()  # upper to allow for lower case
-            if wheel_choice in list(self.wheel_look_up):
-                return wheel_choice, self.get_wheel_from_wheel_id(wheel_choice)
+            wheel_choice_id = input("What wheel would you like to play on?\n"
+                                    f"{wheel_options_text}\n--->").upper()  # upper to allow for lower case
+            if wheel_choice_id in list(self.wheel_look_up):
+                wheel_choice_object = self.get_wheel_from_wheel_id(wheel_choice_id)
+                return wheel_choice_id, wheel_choice_object
             else:
                 print("Invalid wheel choice, please try again")
 
-    def choose_bet(self, wheel_id: str) -> RouletteBet:
+    def choose_bet(self, wheel_id: str) -> USER_BET_TYPES:
         """
         Method that navigates the user to choose their bet, by applying the choose_playing_wheel,
         choose_bet_category and then the choose_bet_type methods below.
@@ -52,11 +59,12 @@ class WheelAndBetTypeSelectorUser(WheelAndBetTypeSelector):
             else:
                 print("Not a valid bet category, try again")
 
-    def choose_bet_type(self, wheel_id: str, bet_cat: str) -> RouletteBet:
+    def choose_bet_type(self, wheel_id: str, bet_cat: str) -> USER_BET_TYPES:
         while True:
-            bet_type_id = input("What category of bet would you like to place?"
+            bet_type_id = input("What type of bet would you like to place?"
                                 f"\n{bet_type_options_text[wheel_id][bet_cat]}\n--->").upper()
             if bet_type_id in bet_cats_and_types[wheel_id][bet_cat]:
-                return self.get_bet_type_from_bet_type_id(bet_type_id)
+                bet_type = self.get_bet_type_from_bet_type_id(bet_type_id)
+                return bet_type
             else:
                 print("Not a valid bet type, try again")
