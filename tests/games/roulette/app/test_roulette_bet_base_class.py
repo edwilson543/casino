bae4146e1af42d1480c77e7fd8026a5b36835768
edwilson543 <edward.e.j.wilson@gmail.com>
2022-04-27 18:26:29 +0100

    Updated tests for RouletteBet base class

--- tests/games/roulette/app/test_roulette_bet_base_class.py
+++ tests/games/roulette/app/test_roulette_bet_base_class.py
@@ -1,4 +1,3 @@
-from games.roulette.app.roulette_bet_base_class import RouletteBet
 from games.roulette.definitions.bet_type_defns import ColoursBet
 from games.roulette.definitions.wheel_defns import EuroWheel
 from games.roulette.app.roulette_wheel_base_class import wheel_spin_return
@@ -6,6 +5,7 @@ from games.roulette.definitions.bet_parameters import BetParameters
 
 
 #  TODO how to structure these?
+# Note that ColoursBet used throughout testing, but it's tests are for RouletteBet methods
 
 
 class TestRouletteBet:
@@ -20,31 +20,31 @@ class TestRouletteBet:
     def test_set_min_max_bet(self):
         """Test to check if the look up and setting of min/max bet parameters is working"""
         test_bet_one = ColoursBet(playing_wheel=EuroWheel(), bet_type_id='C')
-        expected_min_bet = BetParameters.E.ColoursBet.min_bet  # C.E. => ColoursBetEuroWheel
-        expected_max_bet = BetParameters.E.ColoursBet.max_bet
+        expected_min_bet = BetParameters.E.COLOURSBET.min_bet  # C.E. => ColoursBetEuroWheel
+        expected_max_bet = BetParameters.E.COLOURSBET.max_bet
         test_bet_one.set_min_max_bet()
         actual_min_bet = test_bet_one.min_bet
         actual_max_bet = test_bet_one.max_bet
         assert expected_min_bet == actual_min_bet and expected_max_bet == actual_max_bet
 
     def test_calculate_payout(self):
-        test_bet = RouletteBet(min_bet=5, max_bet=20, bet_type_id='S', stake=20,
-                               bet_choice=1, win_criteria=[1], payout=720, playing_wheel=EuroWheel())
+        test_bet = ColoursBet(bet_type_id='S', min_bet=5, max_bet=20, stake=20,
+                              bet_choice=1, win_criteria=[1], payout=720, playing_wheel=EuroWheel())
         expected_payout = 720  # as the euro wheel has 36 unbiased slots, bet choice covers 1 slot, stake is 20
         calculated_payout = test_bet.calculate_payout()
         assert calculated_payout == expected_payout
 
     def test_evaluate_bet_winning(self):
-        test_bet_two = RouletteBet(min_bet=5, max_bet=20, bet_type_id='S', stake=20,
-                                   bet_choice=1, win_criteria=[1], payout=720, playing_wheel=EuroWheel())
+        test_bet = ColoursBet(bet_type_id='S', min_bet=5, max_bet=20, stake=20,
+                              bet_choice=1, win_criteria=[1], payout=720, playing_wheel=EuroWheel())
         expected_winnings = 720  # as payout is set to 36 (which is derived from calculate_payout)
         spin_outcome = wheel_spin_return(number_return=1, colour_return="")
-        calculated_winnings = test_bet_two.evaluate_bet(spin_outcome)
+        calculated_winnings = test_bet.evaluate_bet(spin_outcome)
         assert calculated_winnings == expected_winnings
 
     def test_evaluate_bet_losing(self):
-        test_bet = RouletteBet(min_bet=5, max_bet=20, bet_type_id='S', stake=20,
-                               bet_choice=1, win_criteria=[1], payout=720, playing_wheel=EuroWheel())
+        test_bet = ColoursBet(bet_type_id='S', min_bet=5, max_bet=20, stake=20,
+                              bet_choice=1, win_criteria=[1], payout=720, playing_wheel=EuroWheel())
         expected_winnings = 0  # as bet has lost
         spin_outcome = wheel_spin_return(number_return=2, colour_return="")
         calculated_winnings = test_bet.evaluate_bet(spin_outcome)
