06a7c9efe797f9164e99c4fa667658746e2b1285
edwilson543 <edward.e.j.wilson@gmail.com>
2022-05-10 18:33:04 +0100

    Updated player base class unit tests to use fixtures

--- tests/unit_tests/games/test_player_base_class.py
+++ tests/unit_tests/games/test_player_base_class.py
@@ -1,25 +1,34 @@
 from games.player_base_class import Player
 from datetime import datetime
-
-test_player_one = Player(name="test", username="test", password="tst123", active_pot=1000, total_active_stake=0,
-                         last_top_up_datetime=datetime(2022, 4, 7, 2, 1),
-                         active_session_initial_pot=1000, active_session_start_time=datetime(2022, 5, 1, 3, 4),
-                         active_session_top_ups=0, last_session_end_time=datetime(2022, 4, 7, 2, 1))
-
-test_player_one_copy = Player(name="test", username="test", password="tst123", active_pot=1000, total_active_stake=0,
-                              last_top_up_datetime=datetime(2022, 4, 7, 2, 1),
-                              active_session_initial_pot=1000, active_session_start_time=datetime(2022, 5, 1, 3, 4),
-                              active_session_top_ups=0, last_session_end_time=datetime(2022, 4, 7, 2, 1))
-
-test_player_two = Player(name="test_two", username="test_two", password="tst131", active_pot=100, total_active_stake=0,
-                         last_top_up_datetime=datetime(2022, 3, 7, 2, 1),
-                         active_session_initial_pot=1000, active_session_start_time=datetime(2022, 5, 1, 3, 4),
-                         active_session_top_ups=0, last_session_end_time=datetime(2022, 4, 7, 2, 1))
+import pytest
 
 
 class TestPlayer:
-    def test_player_equivalence_true(self):
+    """Class for conducting unit tests on Player base class"""
+
+    @pytest.fixture(scope="class")
+    def test_player_one(self):
+        return Player(name="test", username="test", password="tst123", active_pot=1000, total_active_stake=0,
+                      last_top_up_datetime=datetime(2022, 4, 7, 2, 1),
+                      active_session_initial_pot=1000, active_session_start_time=datetime(2022, 5, 1, 3, 4),
+                      active_session_top_ups=0, last_session_end_time=datetime(2022, 4, 7, 2, 1))
+
+    @pytest.fixture(scope="class")
+    def test_player_one_copy(self):
+        return Player(name="test", username="test", password="tst123", active_pot=1000, total_active_stake=0,
+                      last_top_up_datetime=datetime(2022, 4, 7, 2, 1),
+                      active_session_initial_pot=1000, active_session_start_time=datetime(2022, 5, 1, 3, 4),
+                      active_session_top_ups=0, last_session_end_time=datetime(2022, 4, 7, 2, 1))
+
+    @pytest.fixture(scope="class")
+    def test_player_two(self):
+        return Player(name="test_two", username="test_two", password="tst131", active_pot=100, total_active_stake=0,
+                      last_top_up_datetime=datetime(2022, 3, 7, 2, 1),
+                      active_session_initial_pot=1000, active_session_start_time=datetime(2022, 5, 1, 3, 4),
+                      active_session_top_ups=0, last_session_end_time=datetime(2022, 4, 7, 2, 1))
+
+    def test_player_equivalence_true(self, test_player_one, test_player_one_copy):
         assert test_player_one == test_player_one_copy
 
-    def test_player_equivalence_false(self):
+    def test_player_equivalence_false(self, test_player_one, test_player_two):
         assert test_player_one != test_player_two
